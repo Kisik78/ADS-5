104bed3e4b2b575e44941b994b12bae9257b1bc4
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-04-03 11:39:03 +0300

    Konkov

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,29 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+     T arr[100];
+     int top;
+ public:
+     TStack() : top(-1) {}
+     bool isEmpty() const {
+         return top == -1;
+     }
+     bool isFull() const {
+         return top == size - 1;
+     }
+     T get() {
+         return arr[top];
+     }
+     void pop() {
+         if (top >= 0)
+             top--;
+     }
+     void push(T value) {
+         if (top < size - 1)
+             arr[++top] = value;
+     }
 };
-
 #endif  // INCLUDE_TSTACK_H_
