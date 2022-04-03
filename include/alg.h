28e88fad2357ae5ac085912fa7fb219b7a649ada
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-04-03 11:54:56 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -1,29 +1,10 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-template<typename T, int size>
-class TStack {
- private:
-     T arr[100];
-     int top;
- public:
-     TStack() :top(-1) { }
-    T get() const {
-        return arr[top];
-    }
-    bool isEmpty() const {
-        return top == -1;
-    }
-    bool isFull() const {
-        return top == size - 1;
-    }
-    void pop() {
-        if (top >= 0)
-            top--;
-    }
-    void push(T item) {
-        if (top < size - 1)
-            arr[++top] = item;
-    }
-};
-#endif  // INCLUDE_TSTACK_H_
+#ifndef INCLUDE_ALG_H_
+#define INCLUDE_ALG_H_
+
+#include <string>
+
+std::string infx2pstfx(std::string);
+int eval(std::string);
+
+#endif  // INCLUDE_ALG_H_
